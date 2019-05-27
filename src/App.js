9755eecc8133da4060894399da345f0b534a90fa
Nils Hartmann <nils@nilshartmann.net>
2019-05-27 23:25:13 +0200

    examples

--- src/App.js
+++ src/App.js
@@ -1,25 +1,182 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { CSSTransition } from "react-transition-group";
+import TabBar from "./TabBar";
+import { HashRouter as Router } from "react-router-dom";
+
+function Block() {
+  const [state, setState] = React.useState(false);
+
+  const className = state ? "Block-open" : "Block-closed";
+
+  return (
+    <>
+      <p>
+        Der folgende Block ist <b>immer</b> im DOM und bekommt durch klicken nur
+        andere Farbe durch neu-setzen der CSS Klasse:
+      </p>
+      <div className={className}>
+        <p>eins</p>
+        <p>zwei</p>
+        <p>drei</p>
+      </div>
+      <button onClick={e => setState(!state)}>Toggle</button>
+    </>
+  );
+}
+
+function AppearBlock() {
+  const [state, setState] = React.useState(false);
+
+  const className = state ? "AppearBlock-open" : "AppearBlock-closed";
+
+  return (
+    <>
+      <p>
+        Der folgende Block ist <b>immer</b> im DOM und bekommt durch klicken nur
+        andere Opacity durch neu-setzen der CSS Klasse:
+      </p>
+      <div className={className}>
+        <p>eins</p>
+        <p>zwei</p>
+        <p>drei</p>
+      </div>
+      <button onClick={e => setState(!state)}>Toggle</button>
+    </>
+  );
+}
+
+function UnmountBlock() {
+  const [state, setState] = React.useState(false);
+
+  const className = state ? "UnmountBlock-open" : "UnmountBlock-closed";
+
+  return (
+    <>
+      <p>
+        Der folgende Block ist nur im DOM, wenn state auf true gesetzt ist.
+        Opacity wird zwar gesetzt, hat aber keine Auswirkung, weil sich kein CSS
+        Property ändert (Element wird nur ein- ausgeblendet, aber nicht{" "}
+        <em>verändert</em>)
+      </p>
+      {state && (
+        <div className={className}>
+          <p>eins</p>
+          <p>zwei</p>
+          <p>drei</p>
+        </div>
+      )}
+      <button onClick={e => setState(!state)}>
+        Toggle (aktuell sichtbar: {String(state)})
+      </button>
+    </>
+  );
+}
+
+function ManualTransition() {
+  const [state, setState] = React.useState("HIDDEN");
+  // eslint-disable-next-line
+  React.useEffect(() => {
+    if (state === "START") {
+      setState("OPEN");
+    }
+  });
+
+  let className = "";
+  switch (state) {
+    case "HIDDEN":
+      break;
+    case "START":
+      className = "ManualTransition-onMount";
+      break;
+    case "OPEN":
+      className = "ManualTransition-show";
+      break;
+    default:
+  }
+
+  return (
+    <>
+      <p>
+        Der folgende Block ist nur im DOM, wenn state auf START oder OPEN
+        gesetzt ist. Im Zustand START wird das Element eingeblendet, mit einer
+        CSS Klasse die die Opacity initial setzt, so dass das Element zunächst
+        unsichtbar ist. Unmittelbar nach dem Rendern im DOM wird dann die CSS
+        Klase auf "OPEN" gesetzt, und die Transition auf Opacity 1 beginnt.
+      </p>
+      {state !== "HIDDEN" && (
+        <div className={className}>
+          <p>eins</p>
+          <p>zwei</p>
+          <p>drei</p>
+        </div>
+      )}
+      <button onClick={e => setState("START")}>
+        Toggle (aktuell sichtbar: {state})
+      </button>
+    </>
+  );
+}
+
+function ReactTransition() {
+  const [state, setState] = React.useState(false);
+
+  return (
+    <>
+      <p>
+        Der folgende Block ist nur im DOM, wenn state auf true gesetzt ist. Das
+        Setzen der Klassen beim Erscheinen und Verschwinden regelt die
+        CSSTransitionGroup
+      </p>
+      <CSSTransition
+        appear
+        in={state}
+        timeout={1000}
+        classNames="ReactTransition"
+        unmountOnExit
+      >
+        <div>
+          <p>eins</p>
+          <p>zwei</p>
+          <p>drei</p>
+        </div>
+      </CSSTransition>
+      <button onClick={e => setState(!state)}>
+        Toggle (aktuell sichtbar: {String(state)})
+      </button>
+    </>
+  );
+}
+
+function Wrapper(props) {
+  return <div className="Wrapper">{props.children}</div>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Wrapper>
+        <TabBar />
+      </Wrapper>
+      <Wrapper>
+        <Block />
+      </Wrapper>
+
+      <Wrapper>
+        <AppearBlock />
+      </Wrapper>
+
+      <Wrapper>
+        <UnmountBlock />
+      </Wrapper>
+
+      <Wrapper>
+        <ManualTransition />
+      </Wrapper>
+      <Wrapper>
+        <ReactTransition />
+      </Wrapper>
+    </Router>
   );
 }
 
---
+++ src/TabBar.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { Route, Link } from "react-router-dom";
+import { CSSTransition } from "react-transition-group";
+function Eins() {
+  return (
+    <div>
+      <h1>Eins</h1>
+      <p>Lorem ipsum</p>
+    </div>
+  );
+}
+
+function Zwei() {
+  return (
+    <div>
+      <h1>Zwei</h1>
+      <p>Lirum larum löffelstiel</p>
+    </div>
+  );
+}
+
+export default function TabBar() {
+  return (
+    <>
+      <div className="TabBar">
+        <Link to="/eins">Eins</Link>
+        <Link to="/zwei">Zwei</Link>
+      </div>
+      <Route path="/eins">
+        {({ match }) => (
+          <CSSTransition
+            in={match != null}
+            timeout={1000}
+            classNames="tab"
+            unmountOnExit
+          >
+            <div className="tab">
+              <Eins />
+            </div>
+          </CSSTransition>
+        )}
+      </Route>
+
+      <Route path="/zwei">
+        {({ match }) => (
+          <CSSTransition
+            in={match != null}
+            timeout={1000}
+            classNames="tab"
+            unmountOnExit
+          >
+            <div className="tab">
+              <Zwei />
+            </div>
+          </CSSTransition>
+        )}
+      </Route>
+    </>
+  );
+}
